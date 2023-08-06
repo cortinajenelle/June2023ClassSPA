703fdd8e871250392f0eb002f4e3ef991362160a
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-08-06 17:44:51 -0400

    Week 8

--- server/app.js
+++ server/app.js
@@ -1,19 +1,30 @@
-// 'Import' the http module
-import http from 'http';
-// Initialize the http server
-const server = http.createServer((request, response) => {
-    // Handle the request from http://localhost:4040/status with HTTP GET method
-    if (request.url === "/status" && request.method === "GET") {
-      // Create the headers for response
-      response.writeHead(200, { "Content-Type": "application/json" });
-      // Create the response body
-      response.write(JSON.stringify({ message: "Service healthy" }));
-      // End and return the response
-      response.end();
-    }
-  })
-  // Tell the HTTP server to start listening
-  .listen(4040);
+// 'Import' the Express module instead of http
+import express from "express";
+// Initialize the Express application
+const app = express();
 
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
+  response.json({ message: "Service healthy" });
+});
+
+app.get("/weather/:city", (request, response) => {
+  // Express adds a "params" Object to requests that has an matches parameter created using the colon syntax
+  const city = request.params.city;
+  // Generate a random number to use as the temperature
+  // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+  const min = 70;
+  const max = 90;
+  const temp = Math.floor(Math.random() * (max - min + 1) + min);
+  // handle GET request for weather with an route parameter of "city"
+  response.json({
+    current: `The weather in ${city} is ${temp} degrees today.`
+  });
+});
+
+// Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-console.log("Listening on port 4040");
+app.listen(4040, () => console.log("Listening on port 4040"));
