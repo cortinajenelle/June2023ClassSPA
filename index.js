785bc14eb4e4b4df62e97babbe462cf7b29c462c
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-08-16 21:42:07 -0400

    SPA

--- index.js
+++ index.js
@@ -27,27 +27,27 @@ function afterRender(state) {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
 
-  if (st.view === "Order") {
+  if (state.view === "Order") {
     // Add an event handler for the submit button on the form
     document.querySelector("form").addEventListener("submit", event => {
       event.preventDefault();
-  
+
       // Get the form element
       const inputList = event.target.elements;
       console.log("Input Element List", inputList);
-  
+
       // Create an empty array to hold the toppings
       const toppings = [];
-  
+
       // Iterate over the toppings array
-  
+
       for (let input of inputList.toppings) {
         // If the value of the checked attribute is true then add the value to the toppings array
         if (input.checked) {
           toppings.push(input.value);
         }
       }
-  
+
       // Create a request body object to send to the API
       const requestData = {
         customer: inputList.customer.value,
@@ -58,12 +58,12 @@ function afterRender(state) {
       };
       // Log the request body to the console
       console.log("request Body", requestData);
-     
+
       axios
         // Make a POST request to the API to create a new pizza
-        .post(`${process.env.PIZZA_PLACE_API_URL}/pizzas`, requestData) 
+        .post(`${process.env.PIZZA_PLACE_API_URL}/pizzas`, requestData)
         .then(response => {
-        //  Then push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+          //  Then push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
           store.Pizza.pizzas.push(response.data);
           router.navigate("/Pizza");
         })
@@ -73,7 +73,6 @@ function afterRender(state) {
         });
     });
   }
-
 }
 
 router.hooks({
@@ -90,19 +89,19 @@ router.hooks({
           .get(
             `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
           )
-          .then((response) => {
-            const kelvinToFahrenheit = (kelvinTemp) =>
+          .then(response => {
+            const kelvinToFahrenheit = kelvinTemp =>
               Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
 
             store.Home.weather = {
               city: response.data.name,
               temp: kelvinToFahrenheit(response.data.main.temp),
               feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
-              description: response.data.weather[0].main,
+              description: response.data.weather[0].main
             };
             done();
           })
-          .catch((err) => {
+          .catch(err => {
             console.log(err);
             done();
           });
@@ -110,11 +109,11 @@ router.hooks({
       case "Pizza":
         axios
           .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
-          .then((response) => {
+          .then(response => {
             store.Pizza.pizzas = response.data;
             done();
           })
-          .catch((error) => {
+          .catch(error => {
             console.log("It puked", error);
             done();
           });
@@ -123,20 +122,20 @@ router.hooks({
         done();
     }
   },
-  already: (params) => {
+  already: params => {
     const view =
       params && params.data && params.data.view
         ? capitalize(params.data.view)
         : "Home";
 
     render(store[view]);
-  },
+  }
 });
 
 router
   .on({
     "/": () => render(store.Home),
-    ":view": (params) => {
+    ":view": params => {
       let view = capitalize(params.data.view);
 
       if (store.hasOwnProperty(view)) {
@@ -145,6 +144,6 @@ router
         render(store.Viewnotfound);
         console.log(`View ${view} not defined`);
       }
-    },
+    }
   })
   .resolve();
